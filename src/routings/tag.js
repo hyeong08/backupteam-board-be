0c26b9ca08fde9d519a9111e5ef53a9d25e8d4d0
hyeong08 <gudtjq9735@gmail.com>
2023-01-30 14:09:10 +0900

    음...

--- src/routings/tag.js
+++ src/routings/tag.js
@@ -7,7 +7,7 @@ const {tags, tagArticles} = require("../controller/tag")
 // 태그 페이지
 router.get("/tags", tags)
 // 해당 태그 게시글 목록
-router.get("/tags/tagId", tagArticles)
+router.get("/tags/:tagId", tagArticles)
 
 
 module.exports = router
